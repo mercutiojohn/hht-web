4dc4fc085e739d737530450765c9716c5214b628
Mercutio <mercutio_john@foxmail.com>
2024-09-17 13:22:15 +0800

    chore: pwa

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,28 @@
-self.addEventListener('install', function (event) {
+const CACHE_NAME = 'offline-cache-v2';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/ico.png',
+  '/manifest.json',
+];
+
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('static-v1').then(function (cache) {
-      return cache.addAll([
-        './',
-        './index.html',
-      ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
 self.addEventListener('activate', function (event) {
+  const cacheWhitelist = [CACHE_NAME]; // 只保留当前的缓存
   event.waitUntil(
     caches.keys().then(function (cacheNames) {
       return Promise.all(
         cacheNames.map(function (cacheName) {
-          return caches.delete(cacheName);
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName); // 删除过时的缓存
+          }
         })
       );
     })
@@ -24,7 +32,7 @@ self.addEventListener('activate', function (event) {
 
 self.addEventListener('fetch', function (event) {
   const requestUrl = new URL(event.request.url);
-  if (/\.(html|css|js)$/.test(requestUrl.pathname)) {
+  if (/\.(html|css|js|png|jpg|jpeg|gif|svg)$/.test(requestUrl.pathname)) {
     event.respondWith(
       caches.match(event.request).then(function (response) {
         return response || fetch(event.request);
